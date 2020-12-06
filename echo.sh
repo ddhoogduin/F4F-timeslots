c881c64cb123f92fd6c714b71310c212799ece35
Dylan Hoogduin <dylanhoogduin@Dylans-MacBook-Pro.local>
2020-12-06 11:41:11 +0100

    change time frame

--- echo.sh
+++ echo.sh
@@ -6,7 +6,7 @@ do
   if test -f "data/new.csv"; then
       mv data/new.csv data/old.csv
       mv data/new_token.csv data/old_token.csv
-      python engine.py --days=1,2,3
+      python engine.py --days=0,1,2
       if cmp -s  data/old_token.csv  data/new_token.csv ; then
         echo "Nothing changed"
       else
@@ -14,7 +14,7 @@ do
         telegram-send --file data/new.csv --caption "Possible slots in coming 3 days"
       fi
   else
-      python engine.py --days=1,2,3
+      python engine.py --days=0,1,2
       if test -f "data/new.csv"; then
         telegram-send --file data/new.csv --caption "Possible slots in coming 3 days"
       fi
